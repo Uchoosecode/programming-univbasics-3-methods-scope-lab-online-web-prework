e13cf2f4b32ffdfd877d84bbdb4d616b2a860eec
Ayako Wiltshire <awittyl44@gmail.com>
2019-10-18 00:03:26 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -10,7 +10,7 @@ def link
   puts "It's Dangerous To Go Alone! Take This.\n"
 end
 
-def any_phrase
+def any_phrase(phrase)
   phrase = "Do A Barrel Roll!\n"
-  puts link + phrase
+  puts phrase
 end
